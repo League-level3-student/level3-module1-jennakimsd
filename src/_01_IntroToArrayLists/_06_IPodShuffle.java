b39e9335c58d7aca3e50a328dc6880aa39cbbfed
jennakimsd <jennakimsd@gmail.com>
2020-05-03 17:27:42 -0700

    I did iPod Shuffle, Integer Stack, and IntroToStack (IP)

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,16 +1,37 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener{
+	ArrayList <Song> s = new ArrayList<Song>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
+		Song walls = new Song("walls.mp3");
+		Song strong = new Song("strong.mp3");
+		Song fineLine = new Song("fineLine.mp3");
+		Song heartbreakWeather = new Song("heartbreakWeather.mp3");
+		Song BW = new Song("BW.mp3");
+		Song watermelonSugar = new Song("watermelonSugar.mp3");
+		s.add(walls);
+		s.add(strong);
+		s.add(fineLine);
+		s.add(heartbreakWeather);
+		s.add(BW);
+		s.add(watermelonSugar);
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -18,10 +39,36 @@ public class _06_IPodShuffle{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		
+		frame.setVisible(true);
+		frame.add(panel);
+		button1.setText("Suprise Me!");
+		button2.setText("Stop Song");
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		panel.add(button1);
+		panel.add(button2);
+		frame.pack();
+		frame.setTitle("iPod Shuffle");
 	}
 	
 	public static void main(String[] args) {
 		new _06_IPodShuffle();
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		Random r = new Random();
+		int random = r.nextInt(s.size());
+		if (buttonPressed == button1) {
+			s.get(random).play();
+		}
+		else if(buttonPressed == button2) {
+			for(int i = 0; i < s.size(); i++) {
+				s.get(i).stop();
+			}
+		}
+	}
 }
+
--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -2,49 +2,55 @@ package _02_Integer_Stack;
 
 public class IntegerStack {
 	//1. create a private array of integers
-	
+	private int[] i;
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		int[] array = new int[0];
+		i = array;
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
-		
+		int[] a = new int[i.length + 1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		a[a.length - 1] = v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for(int p = 0; p < i.length; p++) {
+			a[p] = i[p];
+		}
 		//D. set the member array equal to the new array.
-		
+		i = a;
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int j = i[i.length-1];
 		//B. create a new array that is one element smaller than the member array
-		
+		int[] a = new int[i.length - 1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for(int p = 0; p < a.length; p++) {
+			a[p] = i[p];
+		}
 		//D. set the member array equal to the new array
-		
+		i = a;
 		//E. return the variable you created in step A
-		return 0;
+		return j;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		i = new int[0];
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return i.length;
 	}
 }
